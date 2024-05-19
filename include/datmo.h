cd1fd7c22322b697660de89ecf85b4a00c7f1786
Daniel B <daniel.bannister@proton.me>
2024-05-19 14:24:05 +0200

    Deleted useless dependancies

--- include/datmo.h
+++
@@ -1,9 +0,0 @@
-#include <vl53l7cx_class.h>
-#include <Arduino.h>
-
-#define RES VL53L7CX_RESOLUTION_4X4
-
-#define FREQUENCY_HZ 60
-
-uint16_t minValue(VL53L7CX_ResultsData* Results);
-void performClustering(VL53L7CX_ResultsData* Results, uint16_t cluster_threshold);
